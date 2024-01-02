d849f4ecaae75270c212560513a4ac3565727ca2
chanthaboun <chanthaboun102000@gmail.com>
2024-01-02 13:43:09 +0700

    three commit3

--- src/ApplicationForm/PanelHome.java
+++ src/ApplicationForm/PanelHome.java
@@ -1,5 +1,7 @@
 package ApplicationForm;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 import java.sql.*;
+import javax.swing.JOptionPane;
 
 
 public class PanelHome extends javax.swing.JPanel {
@@ -15,6 +17,10 @@ public class PanelHome extends javax.swing.JPanel {
         id = i;
         name = n;
         status = s;
+        
+        sVGImage1.setSvgImage("image_svg/login.svg", 100, 100);
+        sVGImage2.setSvgImage("image_svg/Cancel.svg", 100, 100);
+        sVGImage2.animation();
     }
 
     @SuppressWarnings("unchecked")
@@ -22,30 +28,68 @@ public class PanelHome extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        sVGImage1 = new ApplicationForm.SVGImage();
+        sVGImage2 = new ApplicationForm.SVGImage();
+        jButton1 = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Lao_SomVang", 0, 20)); // NOI18N
         jLabel1.setText("ຍິນດີຕ້ອນຮັບສູ່ໂປຣແກຣມຂ້າຍເຄື່ອງໜ້າຮ້ານ");
+
+        sVGImage1.setBackground(new java.awt.Color(255, 255, 0));
+
+        sVGImage2.setBackground(new java.awt.Color(255, 51, 51));
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(200, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(sVGImage1, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(97, 97, 97)
+                .addComponent(sVGImage2, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(200, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(311, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap(311, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(200, Short.MAX_VALUE)
+                .addContainerGap(48, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(200, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(sVGImage1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(sVGImage2, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap(150, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        FlatSVGIcon icon = new FlatSVGIcon("image_svg/done.svg");
+        JOptionPane.showMessageDialog(null, "ຂໍ້ມູນຖືກບັນທຶກລົງໃນຖານຂໍ້ມູນສໍາເລັດ", "ສໍາເລັດ", JOptionPane.WIDTH, icon);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private ApplicationForm.SVGImage sVGImage1;
+    private ApplicationForm.SVGImage sVGImage2;
     // End of variables declaration//GEN-END:variables
 }
